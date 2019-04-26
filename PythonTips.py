6f84a39bd752ff8c370094a643b0353bdb4eb4d5
pandasgb <464663951@qq.com>
2019-04-26 13:38:00 +0800

    Update and rename PythonTips to PythonTips.py

--- PythonTips.py
+++ PythonTips.py
@@ -8,8 +8,12 @@ nowformat = now.strftime('%Y-%m-%d %H:%M:%S')
 timedelta = datetime.timedelta(days=0, seconds=0, microseconds=0, milliseconds=0, minutes=0, hours=0, weeks=0)
 timethen = now - timedelta
 
+# 字符串转datetime
+datetime.datetime.strptime('2018-1-1', '%Y-%m-%d')
 
-#按行读取文件
+
+
+# 按行读取文件
 text = []
 fpath = ''
 with open(fpath,'r') as f:
